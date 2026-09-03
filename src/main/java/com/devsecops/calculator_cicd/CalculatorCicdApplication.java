7eb0965829c4f5becac5e122d6babe2a57294893
EdmundoZ12 <edmundozentenor12@gmail.com>
2026-09-03 15:25:26 -0400

    chore: initialize calculator application

---
+++ src/main/java/com/devsecops/calculator_cicd/CalculatorCicdApplication.java
@@ -0,0 +1,13 @@
+package com.devsecops.calculator_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorCicdApplication.class, args);
+	}
+
+}
